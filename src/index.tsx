9c1277cacafba32b1cfa2a47ad4ca27c222a8a78
Fabio <fabiomartins.p@gmail.com>
2022-08-19 17:12:30 -0300

    first version

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Curriculum from './components/curriculum/Curriculum';
 import reportWebVitals from './reportWebVitals';
+import SocialMedia from "./components/socialmedia/SocialMedia"
+import Profile from './components/profile/Profile';
+import Body from './components/body/Body';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <header>
+      <Profile/>
+    </header>
+    <Curriculum>
+      <SocialMedia/>
+      <Body/>
+    </Curriculum> 
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
